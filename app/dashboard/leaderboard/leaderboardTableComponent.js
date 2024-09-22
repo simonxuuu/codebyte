7f22e790a41f1d2e4bfeeac2c74ca384307e3e8a
Andrew Miller <andrewmillerfirm@gmail.com>
2024-09-21 18:18:47 -0700

    gave up

--- app/dashboard/leaderboard/leaderboardTableComponent.js
+++ app/dashboard/leaderboard/leaderboardTableComponent.js
@@ -11,7 +11,7 @@ const LeaderboardTableComponent = ({
     {data && <div className="leaderboardTableComponent">
       <h1 className={`leaderboardTableComponentText ${index+1 == 1 ? 'first' : index+1 == 2 ? 'second' : index+1 == 3 ? 'third' : 'other'}`}>{index+1}</h1><h1 className="leaderboardTableComponentText">{data.email}</h1> <h1 className="leaderboardTableComponentText">{data.xp} xp</h1>
     </div>}
-    {!data && <div className="leaderboardTableComponent emptyTableComponent">
+    {data==null && <div className="leaderboardTableComponent emptyTableComponent">
       <h1 className={`leaderboardTableComponentText empty`}>?</h1>
     </div>}
     </>
