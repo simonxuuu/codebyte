af65fb687248c9f175f31986e82d286aeac035fa
Andrew Miller <andrewmillerfirm@gmail.com>
2024-09-28 13:42:10 -0700

    test

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ export const metadata = {
   keywords: "edCode, learn to code reddit, w3schools python, python deep learning w3schools, factorial python, best way to learn python programming, learn java programming for beginners, coding learning for beginners, tynker learn to code, software engineer learning, learn to code for free, how to learn computer programming for free, object oriented software engineering, python beginner practice problems, coding learning websites, gamified coding, gamified coding learning, python coding learning online, python beginner course, code learn org, duolingo coding, how to start learning coding, how to learn to code, how to learn to code ai, how do i learn to code, how to learn to code fast, how to learn to write code in python, how to learn a coding language, how to learn coding at home for free, how to learn to code html",
   og: {
     image: "/socialmedia2.png"
-  }
- 
+  },
+  image : "/socialmedia2.png"
 };
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
