848a886bcc592dba779b82ce9165256aa152384a
Andrew Miller <andrewmillerfirm@gmail.com>
2024-09-28 13:38:10 -0700

    image ref

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
   title: "edCode",
   description:
     "edCode is the easiest way to learn how to code. Learn everything you will need, from any device, anytime, anywhere.",
-  keywords: "edCode, learn to code reddit, w3schools python, python deep learning w3schools, factorial python, best way to learn python programming, learn java programming for beginners, coding learning for beginners, tynker learn to code, software engineer learning, learn to code for free, how to learn computer programming for free, object oriented software engineering, python beginner practice problems, coding learning websites, gamified coding, gamified coding learning, python coding learning online, python beginner course, code learn org, duolingo coding, how to start learning coding, how to learn to code, how to learn to code ai, how do i learn to code, how to learn to code fast, how to learn to write code in python, how to learn a coding language, how to learn coding at home for free, how to learn to code html"
+  keywords: "edCode, learn to code reddit, w3schools python, python deep learning w3schools, factorial python, best way to learn python programming, learn java programming for beginners, coding learning for beginners, tynker learn to code, software engineer learning, learn to code for free, how to learn computer programming for free, object oriented software engineering, python beginner practice problems, coding learning websites, gamified coding, gamified coding learning, python coding learning online, python beginner course, code learn org, duolingo coding, how to start learning coding, how to learn to code, how to learn to code ai, how do i learn to code, how to learn to code fast, how to learn to write code in python, how to learn a coding language, how to learn coding at home for free, how to learn to code html",
+  og: {
+    image: "/socialmedia2.png"
+  }
  
 };
 
